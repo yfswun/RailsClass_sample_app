130d03c5f85c46dfe78c3533c197142e6a3c7a78
Sylvia Wun <yfswun@yahoo.com>
2014-03-02 00:50:20 -0800

    Lab 3 1-7

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:appName) {'Ruby on Rails Tutorial Sample App'}
+  let(:appName) {'CS 232 Rails Development'}
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -34,6 +34,45 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("#{appName} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('CS 232 Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{appName} | Contact")
+    end
+    it "should have h1 tag with page-title class" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("h1.page-title")
+    end
+    it "should have section tag with main class" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("section.main")
+    end
+    it "should have dl tag" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("dl")
+    end
+    it "should have dt tag" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("dt")
+    end
+    it "should have dd tag" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("dd")
+    end
+    it "should have dt tag in dl tag" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("dl dt")
+    end
+    it "should have dd tag in dl tag" do
+      visit '/static_pages/contact'
+      expect(page).to have_css("dl dd")
     end
   end
 
